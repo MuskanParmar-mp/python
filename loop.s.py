8d5e801419d9bd3977fedbdfa4929f8d9a045df2
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-02-12 22:56:16 +0530

    loopsssss

--- loop.s.py
+++ loop.s.py
@@ -6,3 +6,11 @@ if b > a:
 number = 15
 if number > 0:
   print("The number is positive")  
+
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")  
