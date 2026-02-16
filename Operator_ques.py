14d211dd2bce4bdda91eb168fe982040e82038fa
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-02-16 07:23:52 +0530

    operator-practice-question

---
+++ Operator_ques.py
@@ -0,0 +1,25 @@
+#arthimetic practice question
+# Write a program that takes two numbers as input from a user and prints their sum, difference, product, quotient, and the remainder.
+
+x = int(input("enter a no : "))
+y = int(input("enter a no : "))
+print("sum =",x+y)
+
+a = int(input("enter a no :"))
+b = int(input("enter a no:"))
+difference = a - b
+print("difference = ",difference)
+
+p = 2
+q = 5
+print("product = ", p*q)
+
+
+x = float(input("enter a no : "))
+y = float(input("enter a no : "))
+print("division =",x/y)
+
+
+m  = 10
+n = 3
+print("reminder = ", m%n)
