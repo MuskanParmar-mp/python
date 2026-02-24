f60d4e5c454d37976dbde230f6266b5d9388388d
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-02-24 19:23:14 +0530

    java

--- List.py
+++ List.py
@@ -9,3 +9,10 @@ print(mylist.sort)
 x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
 z = float(3)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
