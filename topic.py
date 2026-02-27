edd5f79efe3d72c5827723bf305e792cde6f7555
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-02-27 08:23:34 +0530

    python

--- topic.py
+++ topic.py
@@ -33,3 +33,17 @@ else:
 
 for i in range(1, 6):
     print(i)    
+
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+num = int(input("Enter number: "))
+
+for i in range(1, 11):
+    print(num, "x", i, "=", num*i)
+
+
+
