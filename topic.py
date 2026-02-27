ac8dbcbcf394963da0158b7e48f6cca858b83f03
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-02-27 08:22:27 +0530

    python

---
+++ topic.py
@@ -0,0 +1,26 @@
+print("Hello, World!") 
+
+
+name = "Muskan"
+age = 20
+price = 99.50
+is_active = True
+
+print(name, age, price, is_active)
+
+
+name = input("Enter your name: ")
+print("Welcome", name)
+
+
+age = int(input("Enter age: "))
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote")
+
+
+
+
+    
