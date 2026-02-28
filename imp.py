edf479c2accd303b925f4504cae1270f89e7adeb
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-02-28 20:37:00 +0530

    python_basic_topic

---
+++ imp.py
@@ -0,0 +1,45 @@
+num = int(input("Enter number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+
+
+
+num = int(input("Enter number: "))
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")    
+
+
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+if a > b:
+    print("a is greater")
+else:
+    print("b is greater")    
+
+
+
+
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+if a > b and a > c:
+    print("a is greatest")
+elif b > c:
+    print("b is greatest")
+else:
+    print("c is greatest") 
