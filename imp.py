5834196967904cc634456c31e169b9f7f551dd42
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-02-28 20:43:19 +0530

    python_basic_topic

--- imp.py
+++ imp.py
@@ -192,3 +192,20 @@ print("Reverse =", text[::-1])
 
 
 
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print("1.Add 2.Sub 3.Mul 4.Div")
+choice = input("Choose option: ")
+
+if choice == "1":
+    print(a + b)
+elif choice == "2":
+    print(a - b)
+elif choice == "3":
+    print(a * b)
+elif choice == "4":
+    print(a / b)
+else:
+    print("Invalid Choice")
+
