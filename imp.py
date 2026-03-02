e2fb1e4114ac78fbbb3f7bdece042e43177977ae
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-03-02 20:38:46 +0530

    py

--- imp.py
+++ imp.py
@@ -244,3 +244,15 @@ if total == num:
     print("Armstrong Number")
 else:
     print("Not Armstrong")
+
+
+
+
+
+n = int(input("Enter n: "))
+total = 0
+
+for i in range(1, n + 1):
+    total += i
+
+print("Sum =", total)
