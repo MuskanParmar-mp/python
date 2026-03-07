69efd6b794483fce20aedce09343467038a09012
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-03-07 15:55:53 +0530

    ff

--- dict.py
+++ dict.py
@@ -10,3 +10,42 @@ thisdict = {
   "year": 1964
 }
 print(thisdict)
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+
+
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+
+
+
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
