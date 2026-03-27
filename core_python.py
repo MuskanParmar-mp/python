734baecb3ecae9fa75a6cf1f77d5c7e6e2df5d69
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-03-27 16:29:35 +0530

    py

---
+++ core_python.py
@@ -0,0 +1,60 @@
+#type-casting
+
+a = 5 
+b = 2.5 
+c = a + b 
+print(c)
+print(type(c))
+
+x = "20"
+y = int(x)
+print(type(y))   #int
+print(y+10)       #30
+
+
+
+a = 5
+b = float(a)
+print(b)
+
+num = 50
+text = str(num)
+print(type(text))
+
+
+#string concatenation
+a = 10 
+b = 20
+print(str(a) + str(b))    #1020
+
+
+
+
+##############################################################################################3
+
+#NUMERIC DATA TYPE
+
+
+#integer number
+a = 10 
+b = -5
+c = 0
+
+x = 15
+y = 25
+print("sum = ", x+y)
+
+
+#float
+pi=3.14
+temp= -2.4
+
+a = 2.4
+b=3.5
+print(a*b)
+
+
+#complex numbers
+c= 2+3j
+print(type(c))
+
